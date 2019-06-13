4f27ea25d008c4044630f19efbac049fe074d062
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-06-13 08:14:44 -0400

    styled header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,10 +3,16 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
+      {/* <hr /> */}
       <section className="name-search">
         <h1 className="name">Violet Dawn</h1>
         <input type="search" />
       </section>
+      <section className="contact-cart">
+        <h4 className="contact">CONTACT INFO</h4>
+        <div>SHOPPING CART"link to"</div>
+      </section>
+      {/* <hr /> */}
     </header>
   )
 }
